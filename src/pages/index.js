1280512469313b5ae68e289213a3c818dee1d1a9
Onur <onurbelek@outlook.com>
2022-07-15 10:58:28 +0100

    added responsive gatsby image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,37 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
-import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 export default function Home({ data }) {
   console.log(data)
+  const image = getImage(data.file.childImageSharp.gatsbyImageData)
 
   return (
     <Layout>
       <section className={styles.header}>
 
         <div>
-          <h1>Onur Belek</h1>
+          <h1>Onur </h1>
           <h3>Full Stack Developer</h3>
-          <p>futureproof graduate</p>
+          <p> graduate</p>
           <Link className={styles.btn} to='/projects'>My Portfolio Projects</Link>
         </div>
-        <StaticImage src="../images/banner.png" alt="bepis" />
+        <GatsbyImage image={image} alt="Banner" />
       </section> 
     </Layout>
 
   )
 }
 
-export const bruh = graphql`
+export const query = graphql`
 query Banner {
   file(relativePath: {eq: "banner.png"}) {
-    id
     childImageSharp {
-      id
-      fluid { 
-        ...GatsbyImageSharpFluid
-      }
+      gatsbyImageData(
+        layout: FULL_WIDTH
+        placeholder: BLURRED
+        formats: [AUTO, WEBP]
+        )
     }
   }
 }
