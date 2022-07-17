eb13685bb7d0d37d22cd8f8b6c52b01e605264b1
Onur <onurbelek@outlook.com>
2022-07-17 11:04:04 +0100

    added gatsby-node.js which generates pages for each markdown project files with graphql queries

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,21 +1,48 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import * as styles from '../styles/project-details'
+import * as styles from '../styles/project-details.css'
+import { graphql } from 'gatsby';
 
-function ProjectDetails() {
+function ProjectDetails({ data }) {
+
+  // grabbing properties from the query
+  const { html } = data.markdownRemark
+  const { title, stack } = data.markdownRemark.frontmatter
+  const image = getImage(data.markdownRemark.frontmatter.featuredImg)
   return (
     <Layout>
         <div className={styles.details}> 
-            <h2>title</h2>
-            <h3>stack</h3>
+            <h2>{title}</h2>
+            <h3>{stack}</h3>
             <div className={styles.featured}>
-                {/* image */}
+                <GatsbyImage image={image} alt="an image about the project"/>
             </div>
-            <div className={styles.html} dangerouslySetInnerHTML={}></div>
+            <div className={styles.html} dangerouslySetInnerHTML={{__html: html}}></div>
         </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+query ProjectDetails($slug: String) {
+  markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+    html
+    frontmatter {
+      stack
+      title
+      featuredImg {
+        childImageSharp {
+          gatsbyImageData(
+          layout: FULL_WIDTH
+          placeholder: BLURRED
+          formats: [AUTO, WEBP]
+          )
+        }
+      }
+    }
+  }
+}
+`
 
 export default ProjectDetails
